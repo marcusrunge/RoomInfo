883222e14a0fc9b7b9a34e249da30e0ae45ad5ea
Marcus Runge <marcus.runge@outlook.com>
2018-09-23 15:03:26 +0200

    Basic Calendar Layout

---
+++ RoomInfo/Models/CalendarWeek.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomInfo.Models
+{
+    public class CalendarWeek : ViewModelBase
+    {
+        ObservableCollection<AgendaItem> _weekDayOne = default(ObservableCollection<AgendaItem>);
+        public ObservableCollection<AgendaItem> WeekDayOne { get => _weekDayOne; set { SetProperty(ref _weekDayOne, value); } }
+
+        ObservableCollection<AgendaItem> _weekDayTwo = default(ObservableCollection<AgendaItem>);
+        public ObservableCollection<AgendaItem> WeekDayTwo { get => _weekDayTwo; set { SetProperty(ref _weekDayTwo, value); } }
+
+        ObservableCollection<AgendaItem> _weekDayThree = default(ObservableCollection<AgendaItem>);
+        public ObservableCollection<AgendaItem> WeekDayThree { get => _weekDayThree; set { SetProperty(ref _weekDayThree, value); } }
+
+        ObservableCollection<AgendaItem> _weekDayFour = default(ObservableCollection<AgendaItem>);
+        public ObservableCollection<AgendaItem> WeekDayFour { get => _weekDayFour; set { SetProperty(ref _weekDayFour, value); } }
+
+        ObservableCollection<AgendaItem> _weekDayFive = default(ObservableCollection<AgendaItem>);
+        public ObservableCollection<AgendaItem> WeekDayFive { get => _weekDayFive; set { SetProperty(ref _weekDayFive, value); } }
+
+        ObservableCollection<AgendaItem> _weekDaySix = default(ObservableCollection<AgendaItem>);
+        public ObservableCollection<AgendaItem> WeekDaySix { get => _weekDaySix; set { SetProperty(ref _weekDaySix, value); } }
+
+        ObservableCollection<AgendaItem> _weekDaySeven = default(ObservableCollection<AgendaItem>);
+        public ObservableCollection<AgendaItem> WeekDaySeven { get => _weekDaySeven; set { SetProperty(ref _weekDaySeven, value); } }
+    }
+}
--- RoomInfo/ViewModels/ScheduleViewModel.cs
+++ RoomInfo/ViewModels/ScheduleViewModel.cs
@@ -1,13 +1,22 @@
 ﻿using System;
-
+using System.Collections.ObjectModel;
 using Prism.Windows.Mvvm;
+using RoomInfo.Models;
 
 namespace RoomInfo.ViewModels
 {
     public class ScheduleViewModel : ViewModelBase
     {
+        ObservableCollection<CalendarWeek> _calendarWeeks = default(ObservableCollection<CalendarWeek>);
+        public ObservableCollection<CalendarWeek> CalendarWeeks { get => _calendarWeeks; set { SetProperty(ref _calendarWeeks, value); } }
+
         public ScheduleViewModel()
         {
+            CalendarWeeks = new ObservableCollection<CalendarWeek>();
+            for (int i = 0; i < 10; i++)
+            {
+                CalendarWeeks.Add(new CalendarWeek());
+            }
         }
     }
 }
