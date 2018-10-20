265ec74a7d14996ba88b7ee9c7ac9f67c6e8d143
Marcus Runge <marcus.runge@outlook.com>
2018-10-20 20:00:10 +0200

    Calculations

--- RoomInfo/Models/IncrementalLoadingCalendarWeeks.cs
+++ RoomInfo/Models/IncrementalLoadingCalendarWeeks.cs
@@ -19,7 +19,7 @@ namespace RoomInfo.Models
                 for (int i = 0; i < count; i++)
                 {
                     CalendarWeek firstCalendarWeek = this.First();
-                    
+
                     Add(CalculateCalendarWeek(firstCalendarWeek));
                 }
                 return new LoadMoreItemsResult { Count = count };
@@ -45,17 +45,46 @@ namespace RoomInfo.Models
                 int year = today.Year;
                 int day = today.Day;
                 DayOfWeek dayOfWeek = today.DayOfWeek;
-                int preOverlap = (day - (int)dayOfWeek) * -1;                
+                int preOverlap = (int)dayOfWeek;
+                int postOverlap = 7 - (int)dayOfWeek;
                 if (preOverlap > 0)
-                {                    
+                {
+                    int j = 1;
                     for (int i = preOverlap; i > 0; i--)
                     {
-                        calendarWeek.WeekDayOneDate = today.Subtract(new TimeSpan(i,0,0,0)).Day.ToString();
+                        if (j == 1) calendarWeek.WeekDayOneDate = today.Subtract(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 2) calendarWeek.WeekDayTwoDate = today.Subtract(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 3) calendarWeek.WeekDayThreeDate = today.Subtract(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 4) calendarWeek.WeekDayFourDate = today.Subtract(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 5) calendarWeek.WeekDayFiveDate = today.Subtract(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 6) calendarWeek.WeekDaySixDate = today.Subtract(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 7) calendarWeek.WeekDaySevenDate = today.Subtract(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        j++;
                     }
                 }
-                else
-                {
 
+                if ((int)dayOfWeek == 1) calendarWeek.WeekDayOneDate = today.Day.ToString();
+                else if ((int)dayOfWeek == 2) calendarWeek.WeekDayTwoDate = today.Day.ToString();
+                else if ((int)dayOfWeek == 3) calendarWeek.WeekDayThreeDate = today.Day.ToString();
+                else if ((int)dayOfWeek == 4) calendarWeek.WeekDayFourDate = today.Day.ToString();
+                else if ((int)dayOfWeek == 5) calendarWeek.WeekDayFiveDate = today.Day.ToString();
+                else if ((int)dayOfWeek == 6) calendarWeek.WeekDaySixDate = today.Day.ToString();
+                else if ((int)dayOfWeek == 7) calendarWeek.WeekDaySevenDate = today.Day.ToString();
+
+                if (postOverlap > 0)
+                {
+                    int j = (int)dayOfWeek + 1;
+                    for (int i = 1; i <= postOverlap; i++)
+                    {
+                        if (j == 1) calendarWeek.WeekDayOneDate = today.Add(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 2) calendarWeek.WeekDayTwoDate = today.Add(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 3) calendarWeek.WeekDayThreeDate = today.Add(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 4) calendarWeek.WeekDayFourDate = today.Add(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 5) calendarWeek.WeekDayFiveDate = today.Add(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 6) calendarWeek.WeekDaySixDate = today.Add(new TimeSpan(i, 0, 0, 0)).Day.ToString();
+                        else if (j == 7) calendarWeek.WeekDaySevenDate = today.Add(new TimeSpan(i, 0, 0, 0)).Day.ToString();                        
+                        j++;
+                    }
                 }
             }
             else
