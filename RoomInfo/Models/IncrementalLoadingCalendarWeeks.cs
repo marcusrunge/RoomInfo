4c2c3c08421d1812c3fc4338d64b088b4049c2e1
Marcus Runge <marcus.runge@outlook.com>
2018-10-20 18:06:50 +0200

    Date Overlap

--- RoomInfo/Models/IncrementalLoadingCalendarWeeks.cs
+++ RoomInfo/Models/IncrementalLoadingCalendarWeeks.cs
@@ -19,15 +19,52 @@ namespace RoomInfo.Models
                 for (int i = 0; i < count; i++)
                 {
                     CalendarWeek firstCalendarWeek = this.First();
-                    Add(new CalendarWeek()
-                    {
-                        
-                    });
+                    
+                    Add(CalculateCalendarWeek(firstCalendarWeek));
                 }
                 return new LoadMoreItemsResult { Count = count };
             });
         }
-                
+
+        private CalendarWeek CalculateCalendarWeek(CalendarWeek firstCalendarWeek)
+        {
+            CalendarWeek calendarWeek = new CalendarWeek
+            {
+                WeekDayOne = new ObservableCollection<AgendaItem>(),
+                WeekDayTwo = new ObservableCollection<AgendaItem>(),
+                WeekDayThree = new ObservableCollection<AgendaItem>(),
+                WeekDayFour = new ObservableCollection<AgendaItem>(),
+                WeekDayFive = new ObservableCollection<AgendaItem>(),
+                WeekDaySix = new ObservableCollection<AgendaItem>(),
+                WeekDaySeven = new ObservableCollection<AgendaItem>()
+            };
+            if (firstCalendarWeek == null)
+            {
+                DateTime today = DateTime.Today;
+                int month = today.Month;
+                int year = today.Year;
+                int day = today.Day;
+                DayOfWeek dayOfWeek = today.DayOfWeek;
+                int preOverlap = (day - (int)dayOfWeek) * -1;                
+                if (preOverlap > 0)
+                {                    
+                    for (int i = preOverlap; i > 0; i--)
+                    {
+                        calendarWeek.WeekDayOneDate = today.Subtract(new TimeSpan(i,0,0,0)).Day.ToString();
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+
+            }
+            return calendarWeek;
+        }
+
         public bool HasMoreItems => 0 < 10000;
     }
 }
