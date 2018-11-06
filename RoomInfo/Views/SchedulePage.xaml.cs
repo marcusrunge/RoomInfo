6312059fc044c601e12ad12b3461158f03598d24
Marcus Runge <marcus.runge@outlook.com>
2018-11-06 23:23:38 +0100

    flyout...

--- RoomInfo/Views/SchedulePage.xaml.cs
+++ RoomInfo/Views/SchedulePage.xaml.cs
@@ -6,6 +6,7 @@ using RoomInfo.ViewModels;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Navigation;
 
 namespace RoomInfo.Views
@@ -13,7 +14,8 @@ namespace RoomInfo.Views
     public sealed partial class SchedulePage : Page
     {
         private ScheduleViewModel ViewModel => DataContext as ScheduleViewModel;
-        
+        private ListView rightTappedListView;
+
         public SchedulePage()
         {
             InitializeComponent();            
@@ -24,6 +26,21 @@ namespace RoomInfo.Views
             FrameworkElement frameworkElement = sender as FrameworkElement;
             var attachedFlyout = Flyout.GetAttachedFlyout(frameworkElement);
             attachedFlyout.ShowAt(frameworkElement);
+        }
+        
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem menuFlyoutItem = sender as MenuFlyoutItem;
+            if (menuFlyoutItem != null && rightTappedListView != null)
+            {
+                var contextFlyout = menuFlyoutItem.ContextFlyout;
+                contextFlyout.ShowAt(rightTappedListView);
+            }
+        }
+
+        private void listView_RightTapped(object sender, Windows.UI.Xaml.Input.RightTappedRoutedEventArgs e)
+        {
+            rightTappedListView = sender as ListView;
         }
     }
 }
