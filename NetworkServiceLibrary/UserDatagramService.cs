f9375eecface74c6ea891b3090aa4aa91e912a9c
Marcus Runge <marcus.runge@outlook.com>
2019-01-12 14:47:58 +0100

    BindServiceNameAsync

--- NetworkServiceLibrary/UserDatagramService.cs
+++ NetworkServiceLibrary/UserDatagramService.cs
@@ -33,13 +33,13 @@ namespace NetworkServiceLibrary
             var datagramSocket = new DatagramSocket();
             var backgroundTaskRegistration = await _backgroundTaskService.Register<UserDatagramBackgroundTask>(new SocketActivityTrigger());
             datagramSocket.EnableTransferOwnership(backgroundTaskRegistration.TaskId, SocketActivityConnectedStandbyAction.DoNotWake);
-            await datagramSocket.BindServiceNameAsync(_applicationDataService.GetSetting<string>("UdpPort"));
             datagramSocket.MessageReceived += async (s, e) =>
               {
                   var roomPackage = new Room() { RoomGuid = _applicationDataService.GetSetting<string>("Guid"), RoomName = _applicationDataService.GetSetting<string>("RoomName"), RoomNumber = _applicationDataService.GetSetting<string>("RoomNumber") };                  
                   var json = JsonConvert.SerializeObject(roomPackage);
                   await _transmissionControlService.SendStringData(e.RemoteAddress, _applicationDataService.GetSetting<string>("TcpPort"), json);
               };
+            await datagramSocket.BindServiceNameAsync(_applicationDataService.GetSetting<string>("UdpPort"));
         }
     }
 }
