08eaef783f5f1d5ba28369bf9b4d5c83077fa50e
Marcus Runge <marcus.runge@outlook.com>
2019-01-12 15:50:52 +0100

    fix in datagram service

--- NetworkServiceLibrary/UserDatagramService.cs
+++ NetworkServiceLibrary/UserDatagramService.cs
@@ -19,27 +19,28 @@ namespace NetworkServiceLibrary
         IApplicationDataService _applicationDataService;
         IBackgroundTaskService _backgroundTaskService;
         ITransmissionControlService _transmissionControlService;
+        DatagramSocket _datagramSocket;
 
         public UserDatagramService(IApplicationDataService applicationDataService, IBackgroundTaskService backgroundTaskService, ITransmissionControlService transmissionControlService)
         {
             _applicationDataService = applicationDataService;
             _backgroundTaskService = backgroundTaskService;
             _transmissionControlService = transmissionControlService;
+            _datagramSocket = new DatagramSocket();
         }
         public async Task StartListenerAsync()
         {
             var window = CoreWindow.GetForCurrentThread();
             var dispatcher = window.Dispatcher;
-            var datagramSocket = new DatagramSocket();
             var backgroundTaskRegistration = await _backgroundTaskService.Register<UserDatagramBackgroundTask>(new SocketActivityTrigger());
-            datagramSocket.EnableTransferOwnership(backgroundTaskRegistration.TaskId, SocketActivityConnectedStandbyAction.DoNotWake);
-            datagramSocket.MessageReceived += async (s, e) =>
+            _datagramSocket.EnableTransferOwnership(backgroundTaskRegistration.TaskId, SocketActivityConnectedStandbyAction.DoNotWake);
+            _datagramSocket.MessageReceived += async (s, e) =>
               {
                   var roomPackage = new Room() { RoomGuid = _applicationDataService.GetSetting<string>("Guid"), RoomName = _applicationDataService.GetSetting<string>("RoomName"), RoomNumber = _applicationDataService.GetSetting<string>("RoomNumber") };                  
                   var json = JsonConvert.SerializeObject(roomPackage);
                   await _transmissionControlService.SendStringData(e.RemoteAddress, _applicationDataService.GetSetting<string>("TcpPort"), json);
               };
-            await datagramSocket.BindServiceNameAsync(_applicationDataService.GetSetting<string>("UdpPort"));
+            await _datagramSocket.BindServiceNameAsync(_applicationDataService.GetSetting<string>("UdpPort"));
         }
     }
 }
