b017ead613ea02e50a30239422562faa8e14da06
Marcus Runge <marcus.runge@outlook.com>
2019-01-21 19:06:16 +0100

    ADDED Date to AgendaItem on InfoPage

---
+++ RoomInfo/Helpers/DateTimeFormatConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace RoomInfo.Helpers
+{
+    public class DateTimeFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ((DateTimeOffset)value).ToString(@"dd.MM.yy hh\:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
