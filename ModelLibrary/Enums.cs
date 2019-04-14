0e60ade8b4f07521b24088aa271961e3d03b97b9
Marcus Runge <marcus.runge@outlook.com>
2019-04-14 12:31:46 +0200

    ADDED Homeoffice

--- ModelLibrary/Enums.cs
+++ ModelLibrary/Enums.cs
@@ -1,6 +1,6 @@
 ﻿namespace ModelLibrary
 {
-    public enum OccupancyVisualState { FreeVisualState, PresentVisualState, AbsentVisualState, BusyVisualState, OccupiedVisualState, LockedVisualState, UndefinedVisualState }
+    public enum OccupancyVisualState { FreeVisualState, PresentVisualState, AbsentVisualState, BusyVisualState, OccupiedVisualState, LockedVisualState, HomeVisualState, UndefinedVisualState }
     public enum PayloadType { Occupancy, Room, Schedule, StandardWeek, RequestOccupancy, RequestSchedule, RequestStandardWeek, IotDim, AgendaItem, AgendaItemId, Discovery, PropertyChanged }
     public enum Language { de_DE, en_US}
 }
